a8f2cf9eb1fd877b065c321c02171196c974e6d4
Ayush Anil Bharsakle <81760629+bayush-9@users.noreply.github.com>
2025-03-16 10:29:56 +0530

    initial commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Ayush Bharsakle on 26/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
